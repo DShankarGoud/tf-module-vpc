b9e5bfdce1098678969be40c3e8cf9bc85f61269
shiva shankar <sivas1475@gmail.com>
2022-12-24 22:08:36 +0530

    JENKINS

--- routes.tf
+++ routes.tf
@@ -10,3 +10,10 @@ resource "aws_route_table_association" "subnet-rt-assoc" {
   subnet_id      = element(aws_subnet.main.*.id, count.index)
   route_table_id = aws_route_table.route.id
 }
+
+resource "aws_route" "default-vpc-rt" {
+  route_table_id         = aws_route_table.route.id
+  destination_cidr_block = data.terraform_remote_state.tgw.outputs.DEFAULT_VPC_CIDR
+  transit_gateway_id     = data.terraform_remote_state.tgw.outputs.TRANSIT_GW
+}
+
